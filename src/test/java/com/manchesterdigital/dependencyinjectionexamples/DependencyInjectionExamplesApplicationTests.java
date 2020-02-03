e7fef96d3058991fbdf871d6e1fd7a70466642cd
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-03 13:53:52 +0000

    Initial commit

---
+++ src/test/java/com/manchesterdigital/dependencyinjectionexamples/DependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manchesterdigital.dependencyinjectionexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
