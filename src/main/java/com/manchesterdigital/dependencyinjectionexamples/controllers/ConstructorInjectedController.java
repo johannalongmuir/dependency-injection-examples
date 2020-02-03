7b274f8efef3cacc11b74da0beeefe2cf2877a99
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-03 15:57:44 +0000

    Update annotations to be able to access beans

--- src/main/java/com/manchesterdigital/dependencyinjectionexamples/controllers/ConstructorInjectedController.java
+++ src/main/java/com/manchesterdigital/dependencyinjectionexamples/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.manchesterdigital.dependencyinjectionexamples.controllers;
 
 import com.manchesterdigital.dependencyinjectionexamples.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
